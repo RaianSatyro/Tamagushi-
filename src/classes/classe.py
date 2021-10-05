1e972a3f7da7a91829d38dd59d180004e478b91b
Raian Satyro <raiansatyro45@gmail.com>
2021-10-05 16:48:11 -0300

    Criado a função que mpdofica fome, humor e saude de acordo com o alimento dado. Feito também cardapio separado da funcao de modificação

--- src/classes/classe.py
+++ src/classes/classe.py
@@ -1,7 +1,7 @@
 from random import randint
 
 class Tamagushi:
-    def __init__(self, nome, fome, saude, idade, humor = 100):
+    def __init__(self, nome, idade, fome = 50, saude = 50, humor = 50):
         self.nome = nome
         self.fome = fome
         self.saude = saude
@@ -13,28 +13,32 @@ class Tamagushi:
         self.nome = str(input('Digite o nome do seu bixinho: '))
         return self.nome
     
-    
     def alterar_fome(self, valor):
         self.fome += valor
         return self.fome
-
         
-    
-    def alterar_saude(self):
-        pass
-    
-    
-    def alterar_idade(self):
-        pass
+    def alterar_saude(self, valor):
+        self.saude += valor
+        return self.saude
+     
         
-
     def brincar(self):
         self.humor += randint(1, 4)
         self.fome -= randint(1, 6)
         return self.humor , self.fome
+    
+    def alterar_humor(self, valor):
+        self.humor += valor
+        return self.humor
 
-novo_bixinho = Tamagushi('generico', 100, 100, 1)
-novo_bixinho.alterar_fome(3)
-print(novo_bixinho.fome)
+
+    def alterar_idade(self):
+        pass
 
 
+
+
+tata = Tamagushi('tata', 1)
+print(tata.saude)
+tata.alterar_saude(-2)
+print(tata.saude)
--- src/functions/function.py
+++ src/functions/function.py
@@ -1,14 +1,16 @@
 import sys
 sys.path.append('C:/Users/rayan/Documents/Dev/pratica/Tamagushi') 
 
-
+from random import randint
 from src.classes.classe import Tamagushi
+
+
 
 def menu():
     pass
 
 
-def alimentacao():
+def cardapio():
     print('''
     ________________________________________________
     ################### Cardápio ###################: 
@@ -22,12 +24,28 @@ def alimentacao():
     7- Comida japonesa
     8- Hamburguer
     _________________________________________________''')
-    escolha = int(input('Escolha com o que você deseja alimentar seu pet: '))
-    while escolha < 1 or escolha > 8 :
-        escolha = int(input('Escolha uma opção valida: '))
-    return escolha
 
 
 def modifica_fome():
-    pass
+    escolha = int(input('Escolha com o que você deseja alimentar seu pet: '))
+    while escolha < 1 or escolha > 8 :
+        escolha = int(input('Escolha uma opção valida: '))
 
+    if escolha == 1:
+        novo_bixinho.alterar_fome(randint(1, 5))
+        novo_bixinho.alterar_humor(0 - randint(3, 4))
+        novo_bixinho.alterar_saude(randint(1, 3))
+    
+    elif escolha == 2:
+        pass
+    
+
+novo_bixinho = Tamagushi('Tata', 1)
+print(f'fome = {novo_bixinho.fome}/100')
+print(f'Humor = {novo_bixinho.humor}/100')
+print(f'Saude = {novo_bixinho.saude}/100')
+cardapio()
+modifica_fome()
+print(f'fome = {novo_bixinho.fome}/100')
+print(f'Humor = {novo_bixinho.humor}/100')
+print(f'Saude = {novo_bixinho.saude}/100')
